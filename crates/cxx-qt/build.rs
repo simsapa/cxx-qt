73b136857e3c19e1758eb9e044e84cfe8ef771a6
Gambhiro <gambhiro@users.noreply.github.com>
2025-12-14 15:30:29 +0000

    builder.flag() and lipo thinning for ios

--- crates/cxx-qt/build.rs
+++ crates/cxx-qt/build.rs
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use cxx_qt_build::CxxQtBuilder;
+use cxx_qt_build::{is_ios_target, thin_generated_fat_library_with_lipo};
 
 fn main() {
     let mut builder = CxxQtBuilder::new().crate_include_root(Some("include".to_owned()));
@@ -27,4 +28,8 @@ fn main() {
     });
 
     builder.build().export();
+
+    if is_ios_target() {
+        thin_generated_fat_library_with_lipo("libcxx-qt-cxxqt-generated.a", "arm64");
+    }
 }
--- crates/qt-build-utils/src/installation/qmake.rs
+++ crates/qt-build-utils/src/installation/qmake.rs
@@ -193,7 +193,8 @@ impl QtInstallation for QtInstallationQMake {
 
             // Ensure that any framework paths are set to -F
             for framework_path in self.qmake_framework_paths() {
-                builder.flag_if_supported(format!("-F{}", framework_path.display()));
+                // NOTE: builder.flag_if_supported() doesn't work when building for iOS.
+                builder.flag(format!("-F{}", framework_path.display()));
                 // Also set the -rpath otherwise frameworks can not be found at runtime
                 println!(
                     "cargo::rustc-link-arg=-Wl,-rpath,{}",
